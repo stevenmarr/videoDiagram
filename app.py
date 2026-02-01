fc9bd155f8b325da5191f66f512ace749fbf7665
Steven Marr <stevenmarr@me.com>
2026-02-01 16:38:36 -0600

    Refactored to MVC structure with separated files

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, extra_files=['templates/index.html', 'static/litegraph.js', 'static/litegraph.css'])
